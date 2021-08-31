6e79130494dc1e52e6de2f912dbba4a88315c0da
aparecidobatista <aparecido.batista@gmail.com>
2021-08-30 21:53:57 -0300

    Primeira Versão

---
+++ src/controller/ThreadId.java
@@ -0,0 +1,28 @@
+package controller;
+
+// extends Thread
+// Parâmetros por construtor
+// método run ()
+
+public class ThreadId extends Thread  {
+	
+	private int idThread;
+	
+	
+	
+
+	public ThreadId(int idThread) {
+		
+		this.idThread = idThread;
+		
+	}
+
+	@Override
+	public void run() {
+		// Só se executa o que está aqui dentro
+		System.out.println(idThread);
+		
+		}
+	
+	
+	}
